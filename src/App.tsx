b4356e155c7e0acdf19f787d9b54b0571960453b
Hyorry <Borieya0619@gmail.com>
2024-05-06 14:57:29 +0900

    login & join pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Join from './pages/Join';
+
 function App() {
-  return <>맥도날드</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/join' element={<Join />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
