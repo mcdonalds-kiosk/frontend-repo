e358bf49737c88c9de02fef89e9602ef8f47c48f
Eunsoo <101039358+EunSo0@users.noreply.github.com>
2024-05-07 10:18:09 +0900

    Revert "feat: login & join"

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Join from './pages/Join';
+import { Route, Routes } from 'react-router-dom';
 import Order from './pages/Order';
-import OrderConfirm from './pages/OrderConfirm';
 import { CartContext } from './context/CartContext';
 import { useState } from 'react';
 import { Cart } from './utility/types';
+import OrderConfirm from './pages/OrderConfirm';
 
 function App() {
   const [cart, setCart] = useState<Cart[]>([]);
   return (
     <CartContext.Provider value={{ cart, setCart }}>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/join' element={<Join />} />
         <Route path='/order' element={<Order />}></Route>
         <Route path='/orderConfirm' element={<OrderConfirm />}></Route>
       </Routes>
@@ -23,4 +18,3 @@ function App() {
 }
 
 export default App;
-
