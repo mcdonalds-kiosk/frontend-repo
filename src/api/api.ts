d8d3622e19648d4e40e4741b18a2056f50710b26
Hyorry <Borieya0619@gmail.com>
2024-05-08 16:32:33 +0900

    fix postData

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,9 +7,14 @@ export const getData = async (url: string) => {
 };
 
 export const postData = async <T>(url: string, body: T): Promise<any> => {
-  return await fetch(`${BASE_URL}${url}`, {
+  const response = await fetch(`${BASE_URL}${url}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body),
-  }).then((response) => { if (response) response.json() });
+  });
+
+  if (!response.ok) {
+    throw new Error('네트워크 요청이 실패했습니다.');
+  }
+  return await response.json();
 };
